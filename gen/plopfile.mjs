664e8b851d0446b74a361b5b2faca72386b0983d
Chris Carrick <chris@crcarrick.dev>
2023-03-09 01:52:40 -0500

    Add a little solution generator

---
+++ gen/plopfile.mjs
@@ -0,0 +1,45 @@
+import path from "path";
+
+export default function (
+  /** @type {import('plop').NodePlopAPI} */
+  plop
+) {
+  plop.setGenerator("solution", {
+    description: "Scaffold a solution to Advent of Code 2022 problem",
+    prompts: [
+      {
+        type: "input",
+        name: "day",
+        message: "Day?",
+        validate: (input) => !isNaN(input),
+      },
+      {
+        type: "choices",
+        name: "part",
+        message: "Part?",
+        default: "a",
+        choices: ["a", "b"],
+      },
+    ],
+    actions: (data) => {
+      const dir = `day_${data.day}${data.part}`;
+
+      return [
+        {
+          type: "add",
+          path: path.join(dir, "Cargo.toml"),
+          templateFile: "templates/Cargo.toml.hbs",
+        },
+        {
+          type: "add",
+          path: path.join(dir, "input.txt"),
+        },
+        {
+          type: "add",
+          path: path.join(dir, "src", "main.rs"),
+          templateFile: "templates/main.rs.hbs",
+        },
+      ];
+    },
+  });
+}
---
+++ gen/templates/main.rs.hbs
@@ -0,0 +1,27 @@
+fn solution(input: &str) -> u32 {
+    return 7;
+}
+
+fn main() {
+    println!("{}", solution(include_str!("../input.txt")))
+}
+
+// github solution
+//
+//
+
+#[cfg(test)]
+mod tests {
+    use super::solution;
+
+    const INPUT: &str = "
+    
+";
+
+    #[test]
+    fn test_solution() {
+        let result = solution(INPUT);
+
+        assert_eq!(result, 7)
+    }
+}
