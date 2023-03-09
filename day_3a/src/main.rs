f6ce878673e9542ccc045b856de1b376780a47fb
Chris Carrick <chris@crcarrick.dev>
2023-03-09 02:32:28 -0500

    Day 3a

--- day_3a/src/main.rs
+++ day_3a/src/main.rs
@@ -1,5 +1,14 @@
-fn solution(input: &str) -> u32 {
-    return 7;
+fn solution(input: &str) -> usize {
+    return input
+        .lines()
+        .map(|l| {
+            let mut chars = ('a'..='z').chain('A'..='Z').into_iter();
+            let (a, b) = l.split_at(l.len() / 2);
+            let common = a.chars().filter(|c| b.contains(*c)).next().unwrap();
+
+            return chars.position(|c| c == common).unwrap() + 1;
+        })
+        .sum();
 }
 
 fn main() {
@@ -8,20 +17,24 @@ fn main() {
 
 // github solution
 //
-//
+// solution was pretty similar except they did not use a string of abcdefg...etc
+// and instead did some byte math (b'b' - b'a' gives you it's position)
 
 #[cfg(test)]
 mod tests {
     use super::solution;
 
-    const INPUT: &str = "
-    
-";
+    const INPUT: &str = "vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw";
 
     #[test]
     fn test_solution() {
         let result = solution(INPUT);
 
-        assert_eq!(result, 7)
+        assert_eq!(result, 157)
     }
 }
