7768c6bcfa9e3f92b393ffb5d344d8b9d5477be4
Chris Carrick <chris@crcarrick.dev>
2023-03-08 22:16:15 -0500

    Learn rust I guess

---
+++ day_1/src/main.rs
@@ -0,0 +1,40 @@
+fn get_input() -> &'static str {
+    return include_str!("../input.txt");
+}
+
+fn main() {
+    let input = get_input();
+    let elves = input.split("\n\n");
+
+    let totals = elves.map(|elf| {
+        return elf
+            .split("\n")
+            .map(|item| item.parse::<i32>().unwrap())
+            .sum();
+    });
+
+    let mut most = 0;
+
+    for total in totals {
+        if total > most {
+            most = total
+        }
+    }
+
+    println!("{}", most)
+}
+
+fn _better() {
+    let input = get_input();
+    let elves = input.split("\n\n");
+    let most = elves
+        .map(|elf| {
+            elf.lines()
+                .map(|line| line.parse::<u32>().unwrap())
+                .sum::<u32>()
+        })
+        .max()
+        .unwrap();
+
+    println!("{}", most)
+}
